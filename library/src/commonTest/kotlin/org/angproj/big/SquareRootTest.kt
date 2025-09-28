005d085753639c797a4ecd68ea3f176f5826b21f
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-09-28 23:26:35 +0200

    Added some unittests for SquareRoot

---
+++ library/src/commonTest/kotlin/org/angproj/big/SquareRootTest.kt
@@ -0,0 +1,60 @@
+package org.angproj.big
+
+import kotlin.test.*
+
+class SquareRootTest {
+
+    @Test
+    fun testSqrt_zero() {
+        val x = BigInt.zero
+        assertEquals(BigInt.zero, x.sqrt())
+    }
+
+    @Test
+    fun testSqrt_one() {
+        val x = BigInt.one
+        assertEquals(BigInt.one, x.sqrt())
+    }
+
+    @Test
+    fun testSqrt_perfectSquare() {
+        val x = bigIntOf(144)
+        assertEquals(bigIntOf(12), x.sqrt())
+    }
+
+    @Test
+    fun testSqrt_nonPerfectSquare() {
+        val x = bigIntOf(20)
+        assertEquals(bigIntOf(4), x.sqrt())
+    }
+
+    @Test
+    fun testSqrtAndRemainder_perfectSquare() {
+        val x = bigIntOf(49)
+        val (sqrt, rem) = x.sqrtAndRemainder()
+        assertEquals(bigIntOf(7), sqrt)
+        assertEquals(BigInt.zero, rem)
+    }
+
+    @Test
+    fun testSqrtAndRemainder_nonPerfectSquare() {
+        val x = bigIntOf(50)
+        val (sqrt, rem) = x.sqrtAndRemainder()
+        assertEquals(bigIntOf(7), sqrt)
+        assertEquals(bigIntOf(1), rem)
+    }
+
+    /*@OptIn(ExperimentalStdlibApi::class)
+    @Test
+    fun testSqrt_largeNumber_hex() {
+        val x = bigIntOf("16BCC41E9000000000".hexToByteArray()) // 100000000000000000000 in hex
+        val expected = bigIntOf("2540BE400".hexToByteArray())    // 10000000000 in hex
+        assertEquals(expected, x.sqrt())
+    }*/
+
+    @Test
+    fun testSqrt_negative_throws() {
+        val x = bigIntOf(-1)
+        assertFailsWith<BigMathException> { x.sqrt() }
+    }
+}
